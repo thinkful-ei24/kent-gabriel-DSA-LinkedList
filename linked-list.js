67e491bbc4de4a5e6868ea0f6add953bc24877b1
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-08 11:16:58 -0500

    Finish insertFirst and insertLast

---
+++ linked-list.js
@@ -0,0 +1,30 @@
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+  insertAt() {}
+}
